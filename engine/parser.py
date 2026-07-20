2f1d23343e6a26f79b314dbe5ef8c0c0feafc30e
BehnamSalami <129120384+BehnamSalami@users.noreply.github.com>
2026-07-20 15:26:27 +0330

    Update parser.py

--- engine/parser.py
+++ engine/parser.py
@@ -3,26 +3,46 @@ import ast
 
 class FunctionParser:
 
-    def __init__(self, code: str):
-        self.code = code
+    def __init__(self, source_code: str):
+
+        self.source_code = source_code
+
+        self.tree = ast.parse(source_code)
 
     def get_functions(self):
-        tree = ast.parse(self.code)
 
         functions = []
 
-        for node in tree.body:
+        for node in self.tree.body:
 
             if isinstance(node, ast.FunctionDef):
 
-                args = []
+                function = {
+
+                    "name": node.name,
+
+                    "parameters": [],
+
+                    "docstring": ast.get_docstring(node),
+
+                    "line": node.lineno
+
+                }
 
                 for arg in node.args.args:
-                    args.append(arg.arg)
 
-                functions.append({
-                    "name": node.name,
-                    "args": args
-                })
+                    function["parameters"].append(
+
+                        {
+
+                            "name": arg.arg,
+
+                            "type": "text"
+
+                        }
+
+                    )
+
+                functions.append(function)
 
         return functions
