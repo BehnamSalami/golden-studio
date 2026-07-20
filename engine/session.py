2bebdf0505c865ec7210feb237a303a429613504
BehnamSalami <129120384+BehnamSalami@users.noreply.github.com>
2026-07-20 21:56:46 +0330

    Update session.py

--- engine/session.py
+++ engine/session.py
@@ -1,26 +1,22 @@
-from engine.controller import EngineController
-
-
 class Session:
+    """
+    نگهداری اطلاعات پروژه جاری
+    """
 
     def __init__(self):
 
-        self.engine = EngineController()
+        self.project = None
+        self.code = ""
+        self.function = None
+        self.form = None
+        self.inputs = {}
+        self.result = None
 
-        self.current_project = None
+    def clear(self):
 
-    def open_project(self, project):
-
-        self.current_project = project
-
-    def load_code(self, code):
-
-        return self.engine.load_code(code)
-
-    def execute(self, values):
-
-        return self.engine.execute(values)
-
-    def close(self):
-
-        self.current_project = None
+        self.project = None
+        self.code = ""
+        self.function = None
+        self.form = None
+        self.inputs = {}
+        self.result = None
