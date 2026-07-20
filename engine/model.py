119f61da02410454f1342cbf2abc644f98027ed2
BehnamSalami <129120384+BehnamSalami@users.noreply.github.com>
2026-07-20 14:29:16 +0330

    Update model.py

--- engine/model.py
+++ engine/model.py
@@ -1,43 +1,31 @@
 from dataclasses import dataclass
 from datetime import datetime
+import uuid
 
 
 @dataclass
 class Project:
-
     id: str
-
     name: str
-
     project_type: str
-
+    description: str
     created_at: str
-
     updated_at: str
-
-    description: str = ""
-
+    version: str = "1.0"
     favorite: bool = False
 
-    version: str = "1.0"
-
-    @staticmethod
-    def create(name, project_type, description=""):
-
+    @classmethod
+    def create(cls, name, project_type, description=""):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        return Project(
-
-            id=now.replace("-", "").replace(":", "").replace(" ", ""),
-
+        return cls(
+            id=str(uuid.uuid4()),
             name=name,
-
             project_type=project_type,
-
+            description=description,
             created_at=now,
-
-            updated_at=now,
-
-            description=description
-
+            updated_at=now
         )
+
+    def touch(self):
+        self.updated_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
