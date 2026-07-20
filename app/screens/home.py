fde62e83fe4dd7e4431eed0fa872e773014d850d
BehnamSalami <129120384+BehnamSalami@users.noreply.github.com>
2026-07-20 17:59:27 +0330

    Update home.py

--- app/screens/home.py
+++ app/screens/home.py
@@ -1,45 +1,66 @@
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.button import MDRaisedButton
-from kivymd.uix.label import MDLabel
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.label import MDLabel
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.card import MDCard
 
 
 class HomeScreen(MDScreen):
 
     def __init__(self, **kwargs):
-
         super().__init__(**kwargs)
 
-        layout = MDBoxLayout(
-
+        root = MDBoxLayout(
             orientation="vertical",
-
-            padding=20,
-
-            spacing=20
-
+            padding=15,
+            spacing=15
         )
 
         title = MDLabel(
-
             text="Golden Studio",
-
             halign="center",
-
             font_style="Headline"
-
         )
 
-        button = MDRaisedButton(
+        root.add_widget(title)
 
-            text="پروژه جدید",
-
-            pos_hint={"center_x": 0.5}
-
+        new_project = MDRaisedButton(
+            text="➕ پروژه جدید",
+            pos_hint={"center_x": .5}
         )
 
-        layout.add_widget(title)
+        root.add_widget(new_project)
 
-        layout.add_widget(button)
+        subtitle = MDLabel(
+            text="پروژه‌های من",
+            font_style="Title",
+            padding=[0,20,0,10]
+        )
 
-        self.add_widget(layout)
+        root.add_widget(subtitle)
+
+        projects = [
+            "تحلیل شرکت تولیدی",
+            "تحلیل شرکت سرمایه گذاری",
+            "ساخت سایت فروشگاهی"
+        ]
+
+        for project in projects:
+
+            card = MDCard(
+                orientation="vertical",
+                padding=15,
+                radius=[15],
+                size_hint_y=None,
+                height=70
+            )
+
+            label = MDLabel(
+                text="📁  " + project
+            )
+
+            card.add_widget(label)
+
+            root.add_widget(card)
+
+        self.add_widget(root)
