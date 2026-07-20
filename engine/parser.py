9303d25bcd8d54e69caa1fc445cd07906843204b
BehnamSalami <129120384+BehnamSalami@users.noreply.github.com>
2026-07-20 11:38:57 +0330

    Update parser.py

--- engine/parser.py
+++ engine/parser.py
@@ -1,1 +1,29 @@
 
+import ast
+
+
+class FunctionParser:
+
+    def __init__(self, code: str):
+        self.code = code
+
+    def get_functions(self):
+        tree = ast.parse(self.code)
+
+        functions = []
+
+        for node in tree.body:
+
+            if isinstance(node, ast.FunctionDef):
+
+                args = []
+
+                for arg in node.args.args:
+                    args.append(arg.arg)
+
+                functions.append({
+                    "name": node.name,
+                    "args": args
+                })
+
+        return functions
