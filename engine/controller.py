9396f8dfeca0c4193f27e09ee05d148d1c7934f4
BehnamSalami <129120384+BehnamSalami@users.noreply.github.com>
2026-07-21 07:21:40 +0330

    Update controller.py

--- engine/controller.py
+++ engine/controller.py
@@ -1,25 +1,69 @@
 from engine.parser import PythonParser
 from engine.form_engine import FormEngine
+from engine.validator import InputValidator
+from engine.runner import PythonRunner
+from engine.session import Session
 
 
 class EngineController:
     """
-    کنترل‌کننده اصلی موتور برنامه
+    کنترل‌کننده اصلی موتور Golden Studio
     """
 
     def __init__(self):
 
         self.parser = PythonParser()
         self.form_engine = FormEngine()
+        self.validator = InputValidator()
+        self.runner = PythonRunner()
+        self.session = Session()
 
     def load_code(self, code: str):
-
         """
-        دریافت کد پایتون
+        تحلیل کد و ساخت فرم
         """
 
         function = self.parser.parse(code)
 
+        self.session.function = function
+        self.session.code = code
+
         form = self.form_engine.build(function)
 
         return form
+
+    def run(self, values):
+        """
+        اجرای تابع
+        """
+
+        function = self.session.function
+
+        validated = self.validator.validate(
+            function,
+            values
+        )
+
+        result = self.runner.execute(
+            self.session.code,
+            function.name,
+            validated
+        )
+
+        self.session.result = result
+
+        return result
+
+    def get_result(self):
+        """
+        آخرین نتیجه اجرا
+        """
+
+        return self.session.result
+
+    def clear(self):
+        """
+        پاک کردن وضعیت موتور
+        """
+
+        self.session = Session()
