e58cb70400244378fe005e1cf5c3def6b02fe186
BehnamSalami <129120384+BehnamSalami@users.noreply.github.com>
2026-07-22 17:27:44 +0330

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,3 @@
+from app.main import GoldenStudioApp
 
-"""
-Golden Studio
-
-نقطه شروع برنامه
-"""
-
-from app.main import GoldenStudio
-
-
-def main():
-    GoldenStudio().run()
-
-
-if __name__ == "__main__":
-    main()
-
+GoldenStudioApp().run()
